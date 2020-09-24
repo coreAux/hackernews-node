59efc8301e55a1c694de27a80b4478f91cfc1265
Mikael Petersen <info@mikaelpetersen.se>
2020-09-24 23:03:33 +0200

    finished tutorial

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-const { GraphQLServer } = require("graphql-yoga");
+const { GraphQLServer, PubSub } = require("graphql-yoga");
 const { PrismaClient } = require("@prisma/client");
+
+const pubsub = new PubSub();
 
 const Query = require("./resolvers/Query");
 const Mutation = require("./resolvers/Mutation");
 const User = require("./resolvers/User");
 const Link = require("./resolvers/Link");
+const Subscription = require("./resolvers/Subscription");
+const Vote = require("./resolvers/Vote");
 
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   User,
   Link,
+  Vote,
 };
 
 const prisma = new PrismaClient();
@@ -22,6 +28,7 @@ const server = new GraphQLServer({
     return {
       ...request,
       prisma,
+      pubsub,
     };
   },
 });
